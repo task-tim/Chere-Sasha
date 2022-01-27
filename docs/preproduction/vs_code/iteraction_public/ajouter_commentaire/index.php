5b9c8fe14f887c586408af6a07f2dd81dcaa376d
CorKarine <70386393+CorKarine@users.noreply.github.com>
2022-01-27 14:06:16 -0500

    ajout des style pour lenvoie de message
    ajout des style pour suivre la maquette

--- docs/preproduction/vs_code/iteraction_public/ajouter_commentaire/index.php
+++ docs/preproduction/vs_code/iteraction_public/ajouter_commentaire/index.php
@@ -6,15 +6,21 @@ require_once 'connexion.php';
 <head>
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://use.typekit.net/uly2xdd.css">
 </head>
 <body>
+    <div class="boite_message">
+    <h1>chère sasha</h1>
+    <div class="bulle">
+    <img src="media/message_bubble.png" alt="bulle message" class="bulleMessage">
+    <p class="texteInvitation">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non condimentum massa. Donec in leo sit amet augue sagittis placerat at vel ipsum. Cras tristique bibendum augue at consequat. In tempor tempus ornare. </p>
+ </div>
  <form action="commenter.php" method="POST">
-<h4>Ajouter un commentaire</h4>
-<textarea rows = "5" cols = "25" name = "commentaire" placeholder="Votre commentaire">
-</textarea><br>
-<input type="submit" name="commenter" value="Commenter"/><br/>
+<input type='text' class="espaceCommentaire" rows = "5" cols = "25" name = "commentaire" placeholder="Écrivez un message à Sasha …">
+<input class="btnEnvoyer" type="submit" name="commenter" value="Envoyer"/>
+</input><br>
+
 </form>
-<br>
-<hr />   
+</div>  
 </body>
-</html>
+</html> 
