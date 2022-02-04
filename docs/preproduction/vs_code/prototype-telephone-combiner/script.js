a7d7709498b42d7da07d4f688f82536b20230cb0
CorKarine <70386393+CorKarine@users.noreply.github.com>
2022-02-04 13:44:50 -0500

    ajout dune page losque que le telephone est innactif
    bug dans le rejouage avec le timer de l'appel

--- docs/preproduction/vs_code/prototype-telephone-combiner/script.js
+++ docs/preproduction/vs_code/prototype-telephone-combiner/script.js
@@ -14,7 +14,25 @@ let temps2 = document.querySelector('#heure');
 let temps3 = document.querySelector('#time');
 let tempsAppel = document.querySelector('#tempsAppel');
 let journee = document.querySelector('#journee');
+let btnMax = document.querySelector('.max');
+let phoneSleep = document.querySelector('.phoneSleep');
+let secondeAppel = 0;
+let minuteAppel = 0;
+function appelTemps() {
+  console.log(secondeAppel);
+  secondeAppel++;
+  if(secondeAppel == 60){
+    secondeAppel = 0;
+    minuteAppel++;
+  }
+  if(secondeAppel < 10){
+    tempsAppel.innerText = "0" + minuteAppel + ":0" + secondeAppel;
+  }else{   
+    tempsAppel.innerText = "0" + minuteAppel + ":" + secondeAppel;
+  }
+  };
 
+// avoir l'heure en direct
 function horloge(){
 let date = new Date();
 let mois = moisArr[date.getMonth()];
@@ -36,20 +54,27 @@ journee.innerText = semaine + " " + jour + " " + mois;
 }
 setInterval(horloge, 1000);
 
-let secondeAppel = 0;
-let minuteAppel = 0;
-
-  
+clearInterval(appelTemps);
 
 
 
+btnMax.addEventListener('click', function(){
+phoneSleep.style.display = "none";
+phone.style.display="block";
+document.body.style.backgroundColor= " #1a004b";
+//si on ignore l'appel
 const ignoreAppel = function(){
   raccroche.style.display="block";  
   phoneCall.style.display="none";
   document.body.style.backgroundColor= "black";
+  setTimeout(function(){
+    phoneSleep.style.display="block";
+    raccroche.style.display="none";
+  }, 2000)
   }
 const timer = setTimeout(ignoreAppel, 20000);
 
+//notification, appel
 setTimeout(function() {
   box3.style.display="block";
 }, 2000);
@@ -65,30 +90,39 @@ setTimeout(function() {
 setTimeout(function() {
   phoneCall.style.display="block";  
   phone.style.display="none";
+  box2.style.display="none";
+  box3.style.display="none";
+  box1.style.display="none";
   document.body.style.backgroundColor= "purple";
 }, 10000);
 
+//click du bouton raccrocher
 btnraccrocher.addEventListener("click", function(){
   raccroche.style.display="block";
   phoneCall.style.display="none";
   document.body.style.backgroundColor= "black";
   clearTimeout(timer);
+  setTimeout(function(){
+    phoneSleep.style.display="block";
+    raccroche.style.display="none";
+  }, 2000)
 })
+
+//click du bouton répondre
 btnrepondre.addEventListener("click", function(){
-  appelTemps = setInterval(() => {
-    secondeAppel++;
-    if(secondeAppel == 60){
-      secondeAppel = 0;
-      minuteAppel = minuteAppel +1;
-    }
-    if(secondeAppel < 10){
-      tempsAppel.innerText = "0" + minuteAppel + ":0" + secondeAppel;
-    }else{   
-      tempsAppel.innerText = "0" + minuteAppel + ":" + secondeAppel;
-    }
-    }, 1000);
+  setInterval(appelTemps, 1000);
   repondu.style.display="block";
   phoneCall.style.display="none";
   document.body.style.backgroundColor= "black";
   clearTimeout(timer);
+    clearInterval(appelTemps, 4000);
+  setTimeout(function(){
+    phoneSleep.style.display="block";
+    raccroche.style.display="none";
+    repondu.style.display="none";
+    secondeAppel = 0;
+    minuteAppel = 0;
+    tempsAppel.innerText = "00:00";
+  }, 4000)
+})
 })
