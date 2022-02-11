c3889f364378bc6eeb0b3f919203c424cf65b7c5
CorKarine <70386393+CorKarine@users.noreply.github.com>
2022-02-10 21:12:08 -0500

    journal semaine 3 et censure PHP

--- docs/preproduction/vs_code/iteraction_public/ajouter_commentaire/commenter.php
+++ docs/preproduction/vs_code/iteraction_public/ajouter_commentaire/commenter.php
@@ -1,8 +1,15 @@
 <?php
 require_once ('connexion.php');
+function filterwords($filtre){
+    $motFiltrer = array('gosh', 'darn', 'poo');
+    for ($i = 0; $i < sizeof($motFiltrer); $i++) {
+     $filtre = preg_replace_callback('/\b' . $motFiltrer[$i] . '\b/i', function($matches){return str_repeat('*', strlen($matches[0]));}, $filtre);
+    }
+    return $filtre;
+   }
+
 $bdd = getBdd();
 $commenter = $bdd->prepare( "INSERT INTO commentaire(Contenu) VALUES(?)");	  
-$commenter->execute(array($_POST['commentaire']));
-
+$commenter->execute(array(filterwords($_POST['commentaire'])));
 header("Location: index.php");
 ?>
