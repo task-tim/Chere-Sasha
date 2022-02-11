75ff1e08d2f07b4e17ad6aca70acf759539fa4c9
CorKarine <70386393+CorKarine@users.noreply.github.com>
2022-02-11 11:48:58 -0500

    nom database et mot de passe mamp

--- docs/preproduction/vs_code/iteraction_public/ajouter_commentaire/connexion.php
+++ docs/preproduction/vs_code/iteraction_public/ajouter_commentaire/connexion.php
@@ -3,7 +3,7 @@
 // Crée et renvoie l'objet PDO associé
 function getBdd()
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=temps;charset=utf8', 'root', 'mysql', array(
+    $bdd = new PDO('mysql:host=localhost;dbname=chere_sasha;charset=utf8', 'root', 'root', array(
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ));
     return $bdd;
